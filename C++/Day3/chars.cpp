e760035e6ca41dd181caa6e7a9cf7c268014c8de
Ersin Tanriverdi@ndus3 <ersitan@gmail.com>
2024-04-26 12:21:50 +0200

    solution part 2 cpp files are added

---
+++ C++/Day3/chars.cpp
@@ -0,0 +1,4 @@
+#include "chars.hpp"
+
+Chars::Chars(unsigned int &lineNo, size_t &pos, char &_char)
+    : lineNo_(lineNo), pos_(pos), char_(_char) {}
---
+++ C++/Day3/numbers.cpp
@@ -0,0 +1,4 @@
+#include "numbers.hpp"
+
+Numbers::Numbers(unsigned int &lineNo, size_t &pos, std::string &sNumber)
+    : lineNo_(lineNo), pos_(pos), sNumber_(sNumber) {}
