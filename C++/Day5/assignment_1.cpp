5c3edd48480e7df2d8a04da380631b3c0e3c934e
Ersin Tanriverdi@ndus3 <ersitan@gmail.com>
2024-05-01 22:35:52 +0200

    solution is started

--- C++/Day5/assignment_1.cpp
+++ C++/Day5/assignment_1.cpp
@@ -1,6 +1,61 @@
 #include "../utils.hpp"
+#include "reader.hpp"
 
-int main(){
-    
+void getSeedsVector(std::ifstream& file, std::vector<std::string>& vSeeds) {
+    std::string line, sSeeds;
+    while (std::getline(file, line)) {
+        size_t posSeeds = line.find("seeds: ");  // find the seeds
+        if (posSeeds != std::string::npos) {
+            std::istringstream iss(line);
+            while (iss >> sSeeds) {
+                if (std::isdigit(sSeeds[0])) {
+                    vSeeds.push_back(
+                        sSeeds);  // if seeds are found, feed them into a vector
+                }
+            }
+        }
+    }
+}
+
+void findKey(std::string& key, std::string& line) {
+    size_t posSemicolon = line.find(':');
+    if (posSemicolon != std::string::npos) {
+        key = line.substr(0, posSemicolon);
+        size_t posMap = key.find("map");
+        if (posMap != std::string::npos) {
+            key = key.substr(0, posMap - 1);  // remove map from the key
+        }
+    }
+}
+
+void getMaps(std::ifstream& file,
+             std::map<std::string, std::vector<std::vector<int>>>& mMap) {
+    std::string line, key, value, word;
+    std::vector<int> vNumbers;
+    std::vector<std::vector<int>> vVNumbers;
+    bool startValues = false;
+    while (std::getline(file, line)) {
+        std::istringstream iss(line);
+        findKey(key, line);
+        while (iss >> word) {
+            if (std::isdigit(word[0])) {
+                vNumbers.push_back(std::stoi(word));
+            }
+            vVNumbers.push_back(vNumbers);
+        }
+        
+        if (!line.size()) {
+            
+            mMap[key] = vVNumbers;
+            vVNumbers.clear();
+        }
+    }
+}
+
+int main() {
+    std::ifstream myFile("input2.txt");
+    std::map<std::string, std::vector<std::vector<int>>> mMap;
+
+    getMaps(myFile, mMap);
     return 0;
 }
---
+++ C++/Day5/reader.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include "../utils.hpp"
+
+class Reader {
+   public:
+    Reader() = default;
+    ~Reader() = default;
+
+    
+
+   private:
+    std::string line_;
+};
