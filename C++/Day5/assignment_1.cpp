0368560c516f040b41c679104e17d943ec68f07c
Ersin Tanriverdi@ndus3 <ersitan@gmail.com>
2024-05-05 20:25:24 +0200

    added generate maps method

--- C++/Day5/assignment_1.cpp
+++ C++/Day5/assignment_1.cpp
@@ -3,22 +3,6 @@
 
 typedef std::map<std::string, std::vector<std::vector<int>>> myMap;
 
-void getSeedsVector(std::ifstream& file, std::vector<std::string>& vSeeds) {
-    std::string line, sSeeds;
-    while (std::getline(file, line)) {
-        size_t posSeeds = line.find("seeds: ");  // find the seeds
-        if (posSeeds != std::string::npos) {
-            std::istringstream iss(line);
-            while (iss >> sSeeds) {
-                if (std::isdigit(sSeeds[0])) {
-                    vSeeds.push_back(
-                        sSeeds);  // if seeds are found, feed them into a vector
-                }
-            }
-        }
-    }
-}
-
 void findKey(std::string& key, std::string& line) {
     size_t posSemicolon = line.find(':');
     if (posSemicolon != std::string::npos) {
@@ -42,14 +26,14 @@ void getMaps(std::ifstream& file,
         while (iss >> word) {
             if (std::isdigit(word[0])) {
                 vNumbers.push_back(std::stoi(word));
-            }            
+            }
         }
 
-        if(!vNumbers.empty()){
+        if (!vNumbers.empty()) {
             vValue.push_back(vNumbers);
             vNumbers.clear();
         }
-        
+
         if (!line.size()) {
             mMap[key] = vValue;
             vValue.clear();
@@ -57,15 +41,29 @@ void getMaps(std::ifstream& file,
     }
 }
 
-void generateMaps(myMap& mMap){
-    
+void generateMaps(myMap& mMap, std::string& key) {
+    std::map<std::string, std::pair<int, int>> myMap;
+    std::array<int, 100> values{0};
+    auto it = mMap.find(key);
+    //std::vector<int, int> vec;
+    if (it != mMap.end()) {
+        for (auto& e : it->second) {
+            int source = e[1];
+            int destination = e[0];
+            int length = e[2];
+            for (int i = 0; i < length; i++) {
+                values[source + i] = destination + i;
+            }
+            // std::cout<<"de";
+        }
+    }
 }
 
 int main() {
     std::ifstream myFile("input2.txt");
-    
+    std::string key = "seed-to-soil";
     myMap mMap;
     getMaps(myFile, mMap);
-    generateMaps(mMap);
+    generateMaps(mMap, key);
     return 0;
 }
